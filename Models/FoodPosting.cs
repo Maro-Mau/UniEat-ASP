612119a51f631724fd5f6cee12b2f236bc2da965
René Barthel <124598631+Maro-Mau@users.noreply.github.com>
2024-01-17 13:00:19 +0100

    Add project files.

---
+++ Models/FoodPosting.cs
@@ -0,0 +1,12 @@
+﻿namespace UniEat.Models
+{
+    public class FoodPosting
+    {
+        public int Id { get; set; } //Kalenderwoche
+        public string Day { get; set; } //Wochentag
+        public string GerichtA { get; set; } //Gericht A
+        public string GerichtB { get; set; } //Gericht B
+        public string GerichtC { get; set; } //Gericht C
+
+    }
+}
