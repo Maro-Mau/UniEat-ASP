6714fbcf0abb6fb1c9651582c37795ef0af575d7
rene.barthel <rene.barthel@outlook.com>
2024-01-24 17:09:01 +0100

    added: so much stuff Buttons and so :D

---
+++ Controllers/SqlCom.cs
@@ -0,0 +1,31 @@
+﻿using UniEat.Data;
+using Microsoft.AspNetCore.Mvc;
+using UniEat.Data;
+using UniEat.Models;
+
+
+namespace UniEat.Controllers
+{
+    public class SqlCom(ApplicationDbContext context) : Controller
+    {
+        private readonly ApplicationDbContext _context = context;
+    
+     public void DeletFromDB(int Id, _context )
+        {
+
+
+        }
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+}
--- Controllers/UniEatAdminController.cs
+++ Controllers/UniEatAdminController.cs
@@ -1,45 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using UniEat.Models;
-using System.IO;
 using UniEat.Data;
+using UniEat.Models;
 
 namespace UniEat.Controllers
 {
     public class UniEatAdminController : Controller
     {
+        // Hier wird die Datenbank injiziert.
         private readonly ApplicationDbContext _context;
+
         public UniEatAdminController(ApplicationDbContext context)
         {
             _context = context;
         }
-        
-        public IActionResult CreateDishes()
+
+        public IActionResult CreateEditDishes(int Id)
         {
+            // Hier wird ein neues Gericht erstellt und/oder eines aus der Datenbank geladen, um es zu ändern.
+            if (Id != 0)
+            {
+                // Wenn die ID nicht 0 ist, wird ein vorhandenes Gericht aus der Datenbank geladen.
+                var dishFromDb = _context.DishesDatabase.SingleOrDefault(x => x.Id == Id);
+
+                if (dishFromDb != null)
+                {
+                    // Wenn das Gericht gefunden wurde, werden die Daten in die View "addDishToDb" geladen, um sie zu bearbeiten.
+                    return View(dishFromDb);
+                }
+                else
+                {
+                    // Wenn eine ID übergeben wurde, aber das Gericht nicht in der Datenbank existiert, wird ein NotFound-Template angezeigt.
+                    return NotFound();
+                }
+            }
+
+            // Wenn keine ID übergeben wurde, wird angenommen, dass ein neues Gericht angelegt wird.
             return View();
         }
 
         public IActionResult addDishToDB(DishesDbModel dish)
         {
+            // Hier wird dem Gericht ein Besitzer (Owner) hinzugefügt. Aktuell ist es noch der Ersteller, sollte sich jedoch auf eine Rolle ändern, z.B. Admin.
             dish.Owner = User.Identity.Name;
 
-            int id = dish.Id;
-            if(dish.Id == 0)
+            if (dish.Id == 0)
             {
-                id++;
+                // Wenn die ID 0 ist, werden die Daten von "DishesDbModel" in die Datenbank geschrieben.
                 _context.DishesDatabase.Add(dish);
-            }else
+            }
+            else
             {
+                // Wenn die ID nicht 0 ist, werden die Parameter in der Datenbank aktualisiert.
                 _context.DishesDatabase.Update(dish);
             }
-            _context.SaveChanges();
-            return RedirectToAction("CreateDishes");
-        }
 
+            // SaveChanges ist sehr wichtig, da es den Befehl sendet, dass alles, was zuvor passiert ist, gespeichert werden soll.
+            _context.SaveChanges();
+
+            return RedirectToAction("CreateEditDishes");
+        }
 
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult MenuList(int Id)
         {
             return View();
@@ -47,26 +72,46 @@ namespace UniEat.Controllers
 
         public IActionResult PostMenu(LunchPosting lunchPosting)
         {
-            if(lunchPosting.Id == 0)
+            if (lunchPosting.Id == 0)
             {
-            _context.LunchPostss.Add(lunchPosting);
-
+                _context.LunchPostss.Add(lunchPosting);
             }
             else
             {
                 _context.LunchPostss.Update(lunchPosting);
-
             }
-
 
             _context.SaveChanges();
 
             return RedirectToAction("MenuList");
         }
+
         public IActionResult EditDish()
         {
+            // Hier werden nur die Gerichte angezeigt, die dem Besitzer gehören, damit er sie bearbeiten kann.
             var dishFromDB = _context.DishesDatabase.Where(x => x.Owner == User.Identity.Name).ToList();
             return View(dishFromDB);
+        }
+
+        public IActionResult DeleteDish(int Id)
+        {
+            // Wenn die ID 0 ist, wird ein Fehler ausgegeben.
+            if (Id == 0)
+                return BadRequest();
+
+            var dishFromDB = _context.DishesDatabase.SingleOrDefault(x => x.Id == Id);
+
+            // Wenn das Löschen einen Datenbankfehler hat und die ID nicht mehr existiert, wird die Meldung "Not Found" zurückgegeben.
+            if (dishFromDB == null)
+                return NotFound();
+
+            // Hier wird der Löschbefehl an die Datenbank gesendet.
+            _context.DishesDatabase.Remove(dishFromDB);
+
+            // Wieder GANZ wichtig: Speichern!
+            _context.SaveChanges();
+
+            return RedirectToAction("EditDish");
         }
     }
 }
