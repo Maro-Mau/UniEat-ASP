cdeabc9a75026c9cf9398e28c04a4f48cbab19d9
Rene <rene.barthel@outlook.com>
2024-02-07 23:12:47 +0100

    fixDB migration: Update ApplicationDbContextModelSnapshot and add fixDB migration
    This commit updates the ApplicationDbContextModelSnapshot class to set the "ProductVersion" annotation to "8.0.1" instead of "8.0.0". It also adds a new migration called fixDB, which is responsible for updating the database schema.
    
    Additionally, this commit modifies the DishesDbModel class by adding the "required" attribute to the NameOfDish and Owner properties, indicating that these fields are mandatory.
    
    Lastly, it adds a reference to the Newtonsoft.Json package in the UniEat.csproj file with version 13.0.3.

--- Models/DishesDbModel.cs
+++ Models/DishesDbModel.cs
@@ -1,33 +1,20 @@
-﻿namespace UniEat.Models   
+﻿namespace UniEat.Models
 {
     public class DishesDbModel
     {
-        
+
         public int Id { get; set; }
-        public string NameOfDish { get; set; }
+        public required string NameOfDish { get; set; }
         public bool Beef { get; set; }
         public bool Pork { get; set; }
         public bool Chicken { get; set; }
         public bool Veggie { get; set; }
         public bool Fish { get; set; }
-        public string Owner { get; set; }
+        public required string Owner { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime UpdateTime { get; set; }
 
-        public DishesDbModel(int id, string nameOfDish, bool beef, bool pork, bool chicken, bool veggie, bool fish, string owner, DateTime createTime, DateTime updateTime)
-        {
-            Id = id;
-            NameOfDish = nameOfDish;
-            Beef = beef;
-            Pork = pork;
-            Chicken = chicken;
-            Veggie = veggie;
-            Fish = fish;
-            Owner = owner;
-            CreateTime = createTime;
-            UpdateTime = updateTime;
 
-        }
     }
-    
+
 }
