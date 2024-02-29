c8c5759824a1af704248511c5c0a88ef25648261
rene.barthel <rene.barthel@outlook.com>
2024-02-29 12:45:45 +0100

    Update appsettings.json with MongoDB connection details and add new classes for MongoDB settings. Configure UniEatDatabaseSettings in Program.cs. Add UniEatService class for MongoDB operations. Include MongoDB.Driver package in the project file. Update CSS files for styling changes.
    Detailed Description:
    - Added MongoDB connection details to appsettings.json.
    - Created MongoTest and UniEatDatabaseSettings classes.
    - Configured UniEatDatabaseSettings in Program.cs.
    - Added UniEatService class for MongoDB operations.
    - Included the MongoDB.Driver package in the project file.
    - Updated CSS files for styling changes, including background colors and font imports.

---
+++ Models/UniEatDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace UniEat.Models
+{
+    public class UniEatDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string UniEatCollectionName { get; set; } = null!;
+    }
+}
